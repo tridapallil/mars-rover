116cc19040df708bdb3c7a034b1a5c50f112e2e0
Luiz <tridapallil@gmail.com>
2022-10-04 20:44:53 -0300

    Validate missing plateau value

--- src/utils/format-data.js
+++ src/utils/format-data.js
@@ -1,13 +1,19 @@
-import { pick } from 'lodash';
+import { isEmpty, pick } from 'lodash';
+import ValidationError from '../classes/ValidationError';
 
 const parseDataToRovers = (data) => data.map((rover) => (pick(rover, ['coordinates', 'instructions'])));
 
 const formatData = (data) => {
   const upperRight = data.shift();
   const rovers = parseDataToRovers(data);
+  const { plateau } = pick(upperRight, ['plateau']);
+
+  if (isEmpty(plateau)) {
+    throw new ValidationError('Missing plateau input');
+  }
 
   return {
-    upperRight: upperRight.plateau,
+    upperRight: plateau,
     rovers,
   };
 };
